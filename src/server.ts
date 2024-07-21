0d28a50b521faf525538f44ff6de17febc271448
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-21 17:55:43 -0300

    Banco de dados - Configurando o Knex

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
+import { Knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => 'Hello World')
+app.get('/hello', async () => {
+  const tables = await Knex('sqlite_schema').select('*')
+
+  return tables
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server Running')
