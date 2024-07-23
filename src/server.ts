815c3e9206bbf0eef1f79270cb456a65fab39cd9
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-23 18:39:09 -0300

    Banco de dados - Realizando queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
+import { title } from 'node:process'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+  .where('amount', 1000)
+  .select('*')
 
-  return tables
+  return transaction
 })
 
 app.listen({ port: 3333 }).then(() => {
