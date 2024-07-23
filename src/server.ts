92926574fabb13103a6203e0e52f2e45b19d36de
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-23 19:09:03 -0300

    Banco de dados - Tratando env com Zod

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
-import { title } from 'node:process'
+import { env } from './env'
 
 const app = fastify()
 
@@ -13,6 +12,6 @@ app.get('/hello', async () => {
   return transaction
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running on 127.0.0.1:3333')
 })
