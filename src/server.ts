e2a0413b623a1ce833393377cf056c451a2c73b1
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-25 20:46:19 -0300

    Implementando rotas - Criação de transações

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,9 @@ import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.register(transactionRoutes)
+app.register(transactionRoutes, {
+  prefix: 'transactions'
+})
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running on 127.0.0.1:3333')
