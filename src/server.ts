2a3b99aea58a0d3c853d26fdf7a5b65e086c224e
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-25 20:29:54 -0300

    Implementando rotas - Plugins do Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
 import fastify from 'fastify'
 import { knex } from './database'
 import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-  .where('amount', 1000)
-  .select('*')
-
-  return transaction
-})
+app.register(transactionRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running on 127.0.0.1:3333')
