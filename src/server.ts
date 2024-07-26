f360dbdf010be992491aa321239a18c20cfc4ece
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-26 15:53:20 -0300

    Testes automatizados - Testando criação de transação

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
-import fastify from 'fastify'
-import { knex } from './database'
+import {app} from './app'
 import { env } from './env'
-import { transactionRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
 
-const app = fastify()
 
-app.register(cookie)
-
-app.register(transactionRoutes, {
-  prefix: 'transactions'
-})
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running on 127.0.0.1:3333')
