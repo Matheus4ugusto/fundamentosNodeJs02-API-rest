3715d1732f90d34ff1ef8825b949903da75bbb5b
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-26 22:44:26 -0300

    Testes automatizados - Finalizando testes da aplicação

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -47,4 +47,54 @@ describe('Transactions routes', () => {
         
     })
 
+    test('User can list a specific transaction', async () => {
+        const createTransactionResponse = await request(app.server).post('/transactions').send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionResponse = await request(app.server).get('/transactions').set('Cookie', cookies || []).expect(200)
+
+        const transactionId = listTransactionResponse.body.transactions[0].id
+
+        const getTransactionRresponse = await request(app.server)
+          .get(`/transactions/${transactionId}`)
+          .set('Cookie', cookies || [])
+          .expect(200)
+
+        expect(getTransactionRresponse.body.transaction).toEqual(
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000,
+            })
+        )
+        
+    })
+
+    test('User can get the summary', async () => {
+        const createTransactionResponse = await request(app.server).post('/transactions').send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        await request(app.server).post('/transactions').set('Cookie', cookies || []).send({
+            title: 'Debit transaction',
+            amount: 2000,
+            type: 'debit'
+        })
+
+        const summaryResponse = await request(app.server).get('/transactions/summary').set('Cookie', cookies || []).expect(200)
+
+        expect(summaryResponse.body.summary).toEqual({
+            amount: 3000
+        })
+        
+    })
+
 })
