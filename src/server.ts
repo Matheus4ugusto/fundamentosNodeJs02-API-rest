606644cf4b00a09acbf37c039c1f8fc63b39650a
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-07-26 23:56:27 -0300

    fix: render config

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import { env } from './env'
 
 
 
-app.listen({ port: env.PORT }).then(() => {
-  host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost'
+app.listen({ port: env.PORT,   host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost' }).then(() => {
   console.log('HTTP Server Running')
 })
